5bbabb3e47d002c27453b046c06763804c19bf07
jeong-minG <ika818488@gmail.com>
2024-08-23 01:47:08 +0900

    오류수정

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,31 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import ProductCard from '../component/ProductCard';
-import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const ProductAll = () => {
-  const [productList,setProductList]=useState([]);
-const getProducts=async()=>{
-  let url='https://my-json-server.typicode.com/jeong-minG/shopping-db/db'
-  let response = await fetch(url)
-  let data =await response.json()
-  setProductList(data);
-}
-  useEffect(()=>{
-    getProducts()
-  },[])
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = 'https://my-json-server.typicode.com/jeong-minG/shopping-db/db';
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data.products); // data에서 products 배열을 가져와야 합니다.
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div>
       <Container>
-        <Row> 
-        {productList.map((menu) =>
-          <Col lg={3}>
-        <ProductCard item={menu}/>
-        </Col>)
-        } 
-          </Row>
-      </Container> 
+        <Row>
+          {productList.map((menu, index) => ( // key prop 추가
+            <Col lg={3} key={index}> 
+              <ProductCard item={menu} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ProductAll
+export default ProductAll;
