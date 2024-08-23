d789201555c8356ddb4c25a0418365b7e096f51d
jeong-minG <ika818488@gmail.com>
2024-08-24 08:59:15 +0900

    url error

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,45 +1,53 @@
-import React, {useEffect, useState} from 'react'
-import { Container, Row, Col, Button, Dropdown, Alert} from 'react-bootstrap'
-import { useParams } from 'react-router' 
-
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Dropdown } from 'react-bootstrap';
+import { useParams } from 'react-router';
 
 const ProductDetail = () => {
-  let {id} = useParams()
-  const [product,setProduct]=useState(null) 
-  const getProductDetail=async()=>{
-    let url =`https://my-json-server.typicode.com/jeong-minG/shopping-db/db/products/${id}`
+  let { id } = useParams();
+  const [product, setProduct] = useState({}); // 초기값을 빈 객체로 설정
+
+  const getProductDetail = async () => {
+    let url = `https://my-json-server.typicode.com/jeong-minG/shopping-db/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setProduct(data);
-  }
-  useEffect(()=>{
-    getProductDetail()
-  },[])
+  };
+
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+
   return (
     <Container>
       <Row>
         <Col className='product-img'>
-        <img src={product?.img} alt={product?.title}/>
+          <img src={product?.img} alt={product?.title} />
         </Col>
         <Col>
-        <div>{product?.title}</div>
-        <div>￦{product?.price}</div>
-        <Dropdown className="drop-down">
-              <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
-                사이즈 선택
-              </Dropdown.Toggle>
+          <div>{product?.title}</div>
+          <div>￦{product?.price}</div>
+          <Dropdown className="drop-down">
+            <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+              사이즈 선택
+            </Dropdown.Toggle>
 
-              <Dropdown.Menu>
-                {product?.size.length > 0 &&
-                  product.size.map((item) => (
-                    <Dropdown.Item href="#/action-1">{item}</Dropdown.Item>
-                  ))}
-              </Dropdown.Menu>
-            </Dropdown>
+            <Dropdown.Menu>
+              {/* product.size가 배열인지 확인 후 길이 체크 */}
+              {Array.isArray(product.size) && product.size.length > 0 ? (
+                product.size.map((item, index) => (
+                  <Dropdown.Item key={index} href="#/action-1">
+                    {item}
+                  </Dropdown.Item>
+                ))
+              ) : (
+                <Dropdown.Item disabled>사이즈 선택 불가</Dropdown.Item>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
