17b57d6490326eb9bb76b8b34a3b261047947fe2
jeong-minG <ika818488@gmail.com>
2024-08-24 10:32:50 +0900

    bar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-regular-svg-icons'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUser} from '@fortawesome/free-regular-svg-icons'
+import { faMagnifyingGlass, faBars } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = ({ authenticate, setAuthenticate }) => {
     const menuList=['Women','Men','Baby','Kids','H&M HOME','Sport','Sale','지속가능성']
@@ -17,8 +18,37 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
         navigate(`/?q=${keyword}`)
       }
     }
+
+    let [width, setWidth] = useState(0);
   return (
     <div>
+            <div className="side-menu" style={{ width: width }}>
+        <button className="closebtn" onClick={() => setWidth(0)}>
+          &times;
+        </button>
+        <div className="side-menu-list" id="menu-list">
+          {menuList.map((menu, index) => (
+            <button key={index}>{menu}</button>
+          ))}
+        </div>
+      </div>
+      <div className="nav-header">
+        <div className="burger-menu hide">
+          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
+            
+        {authenticate ? (
+          <div onClick={() => setAuthenticate(false)}>
+            <FontAwesomeIcon icon={faUser} />
+            <span style={{ cursor: "pointer" }}>로그아웃</span>
+          </div>
+        ) : (
+          <div onClick={() => navigate("/login")}>
+            <FontAwesomeIcon icon={faUser} />
+            <span style={{ cursor: "pointer" }}>로그인</span>
+          </div>
+        )}
+        </div>
+      </div>
         <div>
         {authenticate ? (
           <div className='login-button' onClick={() => setAuthenticate(false)}>
