e9f19b171e5050be42e4764bf4dcbc8778c09687
jeong-minG <ika818488@gmail.com>
2024-08-24 10:12:55 +0900

    login

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,8 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ authenticate, setAuthenticate }) => {
     const menuList=['Women','Men','Baby','Kids','H&M HOME','Sport','Sale','지속가능성']
     const navigate = useNavigate()
     const goToLogin=()=>{
@@ -18,8 +19,24 @@ const Navbar = () => {
     }
   return (
     <div>
-        <div className='login-button' onClick={goToLogin}><FontAwesomeIcon icon={faUser} /><div>로그인</div></div>
-        <div className='nav-section'><img width={100} src="https://i.ibb.co/xF92kL2/pngegg.png" alt="h&m logo"/></div>
+        <div>
+        {authenticate ? (
+          <div className='login-button' onClick={() => setAuthenticate(false)}>
+            <FontAwesomeIcon icon={faUser} />
+            <span style={{ cursor: "pointer" }}>로그아웃</span>
+          </div>
+        ) : (
+          <div className='login-button' onClick={() => goToLogin()}>
+            <FontAwesomeIcon icon={faUser} />
+            <span style={{ cursor: "pointer" }}>로그인</span>
+          </div>
+        )}
+        </div>
+        <div className='nav-section'>
+        <Link to="/">
+          <img width={100} src="https://i.ibb.co/xF92kL2/pngegg.png" alt="h&m logo"/>
+          </Link>
+          </div>
         <div className='menu-area'>
             <ul className='menu-list'>
                 {menuList.map(menu=>
